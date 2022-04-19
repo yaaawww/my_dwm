87fe39d37e846e54edd540d2178f1d01179f37fe
Jacen-cpu <3163083993@qq.com>
2022-04-19 21:21:33 +0800

    new font size

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 4;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14" , "Symbols Nerd Font:size=14"};
+static const char *fonts[]          = { "monospace:size=14" , "Symbols Nerd Font:size=14", "WenQuanYi Zen Hei Mono:size=14"};
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
